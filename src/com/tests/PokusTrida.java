5ab993403828a821ef3128bae8f39bfc0bd7d005
Mara <polikmage@seznam.cz>
2017-03-22 21:27:54 +0100

    Pridani neceho z notebooku

---
+++ src/com/tests/PokusTrida.java
@@ -0,0 +1,11 @@
+package com.tests;
+
+import org.testng.annotations.*;
+
+/**
+ * Created by Mara on 22.3.2017.
+ */
+public class PokusTrida {
+    // OM pokus
+
+}
---
+++ src/com/tests/PokusTridaTest.java
@@ -0,0 +1,11 @@
+package com.tests;
+
+
+
+/**
+ * Created by Mara on 22.3.2017.
+ */
+public class PokusTridaTest {
+public void pokusVytvorenoNaNotebooku(){}
+
+}
